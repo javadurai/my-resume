d5459e4b6e46cc45e80675de3778429fc62da2c6
Jabadurai Selvaraj <jabadurai.s@gmail.com>
2019-05-25 19:53:37 -0500

    Updates

---
+++ precache-manifest.606985f62cf939b793088dfe47fdd71c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cbd56cca8f5c28e04f0c493c73e38e8",
+    "url": "/javadurai/my-resume/index.html"
+  },
+  {
+    "revision": "9a9c773ffa31f4decaf6",
+    "url": "/javadurai/my-resume/static/css/2.8c8fa9f0.chunk.css"
+  },
+  {
+    "revision": "d9bac472db7f3e949faf",
+    "url": "/javadurai/my-resume/static/css/main.e052ad3f.chunk.css"
+  },
+  {
+    "revision": "9a9c773ffa31f4decaf6",
+    "url": "/javadurai/my-resume/static/js/2.2f3789a4.chunk.js"
+  },
+  {
+    "revision": "d9bac472db7f3e949faf",
+    "url": "/javadurai/my-resume/static/js/main.ad61e157.chunk.js"
+  },
+  {
+    "revision": "151fc07dc412f67342e7",
+    "url": "/javadurai/my-resume/static/js/runtime~main.2bac32f4.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/javadurai/my-resume/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/javadurai/my-resume/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/javadurai/my-resume/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/javadurai/my-resume/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/javadurai/my-resume/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/javadurai/resume/precache-manifest.d655e862a142650db2a772d2e396539d.js"
+  "/javadurai/my-resume/precache-manifest.606985f62cf939b793088dfe47fdd71c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/javadurai/resume/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/javadurai/my-resume/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
