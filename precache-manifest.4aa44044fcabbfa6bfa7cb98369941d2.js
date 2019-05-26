aa4dc5403333d825293953fb4edd9ff07464e55a
Jabadurai Selvaraj <jabadurai.s@gmail.com>
2019-05-25 21:43:17 -0500

    Updates

---
+++ precache-manifest.4aa44044fcabbfa6bfa7cb98369941d2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/javadurai/my-resume/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "289b89e679c4a8e61e39",
+    "url": "/javadurai/my-resume/static/css/main.5d688a23.chunk.css"
+  },
+  {
+    "revision": "151fc07dc412f67342e7",
+    "url": "/javadurai/my-resume/static/js/runtime~main.2bac32f4.js"
+  },
+  {
+    "revision": "ab9ff1af138f918e63f95632f5aa1085",
+    "url": "/javadurai/my-resume/static/media/texture2.ab9ff1af.jpg"
+  },
+  {
+    "revision": "62bef0621f9fe4a40379",
+    "url": "/javadurai/my-resume/static/js/2.cbff8f61.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/javadurai/my-resume/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "289b89e679c4a8e61e39",
+    "url": "/javadurai/my-resume/static/js/main.5999e7de.chunk.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/javadurai/my-resume/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/javadurai/my-resume/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/javadurai/my-resume/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "62bef0621f9fe4a40379",
+    "url": "/javadurai/my-resume/static/css/2.b78eee94.chunk.css"
+  },
+  {
+    "revision": "014cea300a4c290e012b6f6d44882943",
+    "url": "/javadurai/my-resume/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/javadurai/my-resume/precache-manifest.9a5e6e87afabbcc20a3a9d943ce0241b.js"
+  "/javadurai/my-resume/precache-manifest.4aa44044fcabbfa6bfa7cb98369941d2.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/javadurai/my-resume/index.html"), {
+workbox.routing.registerNavigationRoute("/javadurai/my-resume/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
