4c5beb5e1924f052c443fdd43c40eabca28d6225
Jabadurai Selvaraj <jabadurai.s@gmail.com>
2019-05-25 19:21:06 -0500

    Updates

---
+++ precache-manifest.be695e3dfe26229c4f1725ac04f5b07a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84b6b38732b986e5b4a78af253173138",
+    "url": "/javadurai/resume/index.html"
+  },
+  {
+    "revision": "2dce2d0ca2fd9240e722",
+    "url": "/javadurai/resume/static/css/2.295f6dc1.chunk.css"
+  },
+  {
+    "revision": "548a0e594438b245015a",
+    "url": "/javadurai/resume/static/css/main.6f2078a9.chunk.css"
+  },
+  {
+    "revision": "2dce2d0ca2fd9240e722",
+    "url": "/javadurai/resume/static/js/2.ff241af8.chunk.js"
+  },
+  {
+    "revision": "548a0e594438b245015a",
+    "url": "/javadurai/resume/static/js/main.031307c0.chunk.js"
+  },
+  {
+    "revision": "33d8b054010149174fcb",
+    "url": "/javadurai/resume/static/js/runtime~main.d382a5c0.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/javadurai/resume/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/javadurai/resume/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/javadurai/resume/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/javadurai/resume/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/javadurai/resume/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
